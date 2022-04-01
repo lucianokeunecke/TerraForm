0ca74c7161d2c9467b3f58753193d9b57d403635
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-01 06:41:27 -0300

    Trabalho da disciplina

--- security-group.tf
+++ security-group.tf
@@ -15,23 +15,3 @@ resource "aws_security_group" "allow-ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-/*
-resource "aws_security_group" "allow-ssh-oregon" {
-  provider = "aws.oregon"
-  name = "allow-ssh"
-  description = "SG para liberar acesso SSH"
-  ingress {
-    from_port = 22
-    protocol  = "TCP"
-    to_port   = 22
-    cidr_blocks = var.ips_homolog
-  }
-  egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-*/
--- vars.tf
+++ vars.tf
@@ -19,20 +19,3 @@ variable "ips_empresa" {
   type = list
   default = ["179.190.112.206/32", "179.191.112.206/32", "179.192.112.206/32"]
 }
-
-/*
-variable "amis" {
-  type = map
-  default = {
-    ohio: "ami-064ff912f78e3e561",
-    oregon: "ami-0b28dfc7adc325ef4"
-  }
-}
-*/
-
-/*
-variable "ips_homolog" {
-  type = list
-  default = ["191.240.217.145/32","192.240.217.145/32"]
-}
-*/
